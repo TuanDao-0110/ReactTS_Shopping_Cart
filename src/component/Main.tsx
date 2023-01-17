be9faef0eee7fd446292c38e617bdb7fb3efbad0
tuan <rickytuan1991@gmail.com>
2023-01-17 22:16:35 +0200

    add new fn

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ActionType, MyContext, MyContextType } from '../context/StateProvider'
 
 const Main = () => {
+  const { state, dispatch } = useContext<MyContextType>(MyContext)
+
+
   return (
-    <div>main</div>
+    <div>
+      {state.product.map((item, index) => {
+        const { name, incart, price, sku } = item
+        return <div key={index}>
+          {name}  --{incart ? 'bought' : 'not buy'}
+          <button
+          disabled={incart}
+            onClick={() => {
+              dispatch({ type: ActionType.ADD, payload:item})
+            }}
+          >add</button>
+        </div>
+      })}
+    
+      <hr></hr>
+      <div>cart:</div>
+      {state.buyProducts.map((item, index) => {
+        const { name, incart } = item
+        return <div key={index}>
+          {name} is booked
+          <button
+            onClick={() => {
+              dispatch({ type: ActionType.REMOVE, payload: item })
+            }}
+          >remove</button>
+        </div>
+      })}
+    </div>
   )
 }
 
--- src/context/StateProvider.tsx
+++ src/context/StateProvider.tsx
@@ -1,66 +1,101 @@
 import { createContext, useReducer } from "react"
-
+// 1. state use 2 type
+// 2. state type 1
 export interface ProductType {
     sku: string,
     name: string,
-    price: number
+    price: number,
+    incart: boolean
 }
-
+// 3. state type 2
 export interface BuyProductType extends ProductType {
-    quantity: number,
-    total: number
+    quantity?: number,
+    total?: number
 }
+//4. action type
 export interface ActionToCartType {
     type: ActionType,
     payload?: ProductType
 }
-interface MyContextType {
-    state: {
-        product: ProductType[],
-        buyProducts: BuyProductType[]
-    },
+// 5. my context in
+interface StateType {
+    product: ProductType[],
+    buyProducts: BuyProductType[]
+}
+export interface MyContextType {
+    state: StateType
     dispatch: React.Dispatch<ActionToCartType>
 }
-enum ActionType {
+export enum ActionType {
     ADD,
     REMOVE,
 }
 
-const MyContext = createContext<MyContextType>({
-    state: {
-        product: [{
-            "sku": "12345",
-            "name": "item0001",
-            "price": 49.99
-        }, {
-            "sku": "23456",
-            "name": "item0002",
-            "price": 59.99
-        }, {
-            "sku": "34567",
-            "name": "item0003",
-            "price": 69.99
-        }],
-        buyProducts: []
-    },
+const initState: StateType = {
+    product: [{
+        "sku": "12345",
+        "name": "item0001",
+        "price": 49.99,
+        incart: false
+    }, {
+        "sku": "23456",
+        "name": "item0002",
+        "price": 59.99,
+        incart: false
+    }, {
+        "sku": "34567",
+        "name": "item0003",
+        "price": 69.99,
+        incart: false
+    }],
+    buyProducts: []
+}
+
+export const MyContext = createContext<MyContextType>({
+    state: initState,
     dispatch: () => { }
 })
 
 
-interface ProviderProps { 
-    children:React.ReactNode
+interface ProviderProps {
+    children: React.ReactNode
 }
-const reducer = (state :MyContextType,action:ActionToCartType) => { 
-switch (action.type) { 
-    case ActionType.ADD:
-        
-        return {...state,}
-    default : 
-    return state
-}
+const reducer = (state: StateType, action: ActionToCartType): StateType => {
+    switch (action.type) {
+        case ActionType.ADD:
+            if (action.payload) {
+                const { sku } = action.payload
+                const { buyProducts, product } = state
+                const productIndex = product.findIndex(i => i.sku === sku)
+                product[productIndex].incart = true
+                buyProducts.push(product[productIndex])
+            }
+            return { ...state, }
+        case ActionType.REMOVE:
+            if (action.payload) {
+                const { sku } = action.payload
+                const { buyProducts, product } = state
+                const productIndex = product.findIndex(i => i.sku === sku)
+                product[productIndex].incart = false
+                const buyProductsIndex = buyProducts.findIndex(i=> i.sku === sku)
+
+                buyProducts.splice(buyProductsIndex,1)
+            }
+            return { ...state, }
+        default:
+            return state
+    }
 }
 
-const MyProvider = ({children}: ProviderProps)=> { 
+const MyProvider = ({ children }: ProviderProps) => {
+    const [state, dispatch] = useReducer(reducer, initState)
+    const value: MyContextType = { state, dispatch }
+    return <MyContext.Provider value={value}>
+        {children}
+    </MyContext.Provider>
+
 }
+
+export default MyProvider
 
 
